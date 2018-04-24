f4e9f857af1754ba9147eec9f5004ada26d0dcd5
Xuefeng Zhu <xzhu15@illinois.edu>
2018-04-24 09:32:38 -0500

    fix query for dva 2

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
  */
 
 import pathToRegexp from 'path-to-regexp'
+import queryString from 'query-string'
 
 /**
  * @param {Object} history, react-router history
@@ -52,8 +53,9 @@ function subscribe (history, dispatch, ...rules) {
             }
             if (shouldExec) {
                 const queryResult = []
+                const search = queryString.parse(currentLocation.search)
                 for (let query of queries) {
-                    queryResult.push(currentLocation.query[query])
+                    queryResult.push(search[query])
                 }
                 const action = actionCreator && actionCreator(...match.slice(1), ...queryResult)
                 if (Array.isArray(action)) {
